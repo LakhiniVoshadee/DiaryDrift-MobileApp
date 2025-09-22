974bc665cbc39a2d226a157c3e5085e4a399e3ca
Lakhini Voshadee Abeysundara <lakhinivoshadee24@gmail.com>
2025-09-22 13:43:23 +0530

    build the project

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,25 @@
-const { getDefaultConfig } = require("expo/metro-config")
-const { withNativeWind } = require("nativewind/metro")
+const { getDefaultConfig } = require("expo/metro-config");
+const { withNativeWind } = require("nativewind/metro");
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: "./global.css" })
+// Make metro more resilient to missing modules
+const originalResolveRequest = config.resolver.resolveRequest;
+if (originalResolveRequest) {
+  config.resolver.resolveRequest = (context, moduleName, platform) => {
+    try {
+      return originalResolveRequest(context, moduleName, platform);
+    } catch (e) {
+      // If the error is about lightningcss, provide a more friendly error
+      if (e.message && e.message.includes("lightningcss")) {
+        console.warn(
+          `Warning: Missing lightningcss module. Try running 'npm install lightningcss'`
+        );
+        return { type: "empty" };
+      }
+      throw e;
+    }
+  };
+}
+
+module.exports = withNativeWind(config, { input: "./global.css" });
